a2e3706f0ac4e0d8df1b0d70dfbf969831ec0cad
Jun <ofd000j@gmail.com>
2015-07-24 23:08:07 +0900

    memo

--- work2/src/main/java/com/cdg/work2/checker.java
+++ work2/src/main/java/com/cdg/work2/checker.java
@@ -21,5 +21,11 @@ public class checker {
 		return false;
 		//체크에 따라서 다음 토큰의 숫자 정규식을 확인하고, 맞다면 그 다음을 ***로 저장. false인 경우엔 그냥 그대로 저장
 		//아직 idea는 일일히 모든 토큰 다시 저장하는 수밖에없을듯.
+		// phoenix208.blog.me/40036191280
+		/*
+		 * String str = readline
+		 * String changed = str.replaceALL("찾을 문자열", "바꿀 문자열");
+		 * writeFile(f,changed);
+		 */
 	}
 }
